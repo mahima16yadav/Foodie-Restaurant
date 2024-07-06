bc8cfeee21a0696cc948ec19f70251263804b15d
mahima16yadav <mahima1602y@gmail.com>
2024-07-06 16:06:07 +0530

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-background-image-container">
+        <img src={AboutBackground} alt="" />
+      </div>
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          Food Is An Important Part Of A Balanced Diet
+        </h1>
+        <p className="primary-text">
+        Food is essential for maintaining a balanced diet as it provides the necessary nutrients, vitamins, and energy our bodies need to function properly.
+        </p>
+        <p className="primary-text">
+        Consuming a variety of foods ensures we get a balanced intake of proteins, carbohydrates, fats, and minerals, supporting overall health and well-being.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
